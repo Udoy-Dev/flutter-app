7ada806251a3b0695583165db593a2be195c4c94
unknown <udoydas125@gmail.com>
2026-06-17 00:08:36 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
+  }
+}
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        titleSpacing: 0,
+        centerTitle: true,
+        toolbarHeight: 60,
+        elevation: 0,
+        title: Text("Hello Flutter This not",style: TextStyle(color: Colors.white,fontSize: 20),),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.add))
+        ],
+      ),
+      body: Center(
+        child: Text("Hello",style: TextStyle(fontSize: 50),),
+      ),
+    );
+  }
+}
+
